b44e0a2f09ca5ff6e7fc674f38b8189d9eb08bc8
Charles Wang <zwcharl@gmail.com>
2025-03-23 14:34:12 -0400

    init

---
+++ astro.config.ts
@@ -0,0 +1,13 @@
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  output: "server",
+  integrations: [react()],
+  adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});

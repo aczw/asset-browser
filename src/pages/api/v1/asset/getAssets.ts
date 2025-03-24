1f5a09aec2111ab3106c01e39283f4939d7b1d46
Anthony Ge <geant@seas.upenn.edu>
2025-03-24 19:02:40 -0400

    something

---
+++ src/pages/api/v1/asset/getAssets.ts
@@ -0,0 +1,33 @@
+import { 
+    S3Client,
+    ListBucketsCommand
+ } from "@aws-sdk/client-s3";
+
+import type { APIRoute } from "astro";
+
+const S3 = new S3Client({
+    region: "auto",
+    endpoint: import.meta.env.R2_URL,
+    credentials: {
+        accessKeyId: import.meta.env.ACCESS_KEY,
+        secretAccessKey: import.meta.env.SECRET_KEY,
+    },
+});
+
+const GET: APIRoute = async ({url}) => {
+    try {
+        const response = await S3.send(new ListBucketsCommand({}));
+        return new Response(
+            await JSON.stringify(response)
+        );
+    } catch (error)  {
+        console.log(error);
+        const statusText = "bad";
+        return new Response(statusText, {
+            status: 404,
+            statusText,
+        });
+    }
+}
+
+export { GET };

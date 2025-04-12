50d1234a5a849037cee59aea67c384b16663190b
Charles Wang <zwcharl@gmail.com>
2025-04-11 20:20:49 -0400

    display preview as square (b/c our thumbnails are square)

--- src/components/asset-detail/AssetDetailSkeleton.tsx
+++ src/components/asset-detail/AssetDetailSkeleton.tsx
@@ -20,7 +20,7 @@ const AssetDetailSkeleton = () => {
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
         <div className="lg:col-span-7 bg-secondary rounded-lg overflow-hidden">
-          <Skeleton className="aspect-video w-full" />
+          <Skeleton className="aspect-square w-full" />
         </div>
 
         <div className="lg:col-span-5 space-y-6">
--- src/components/asset-detail/AssetPreview.tsx
+++ src/components/asset-detail/AssetPreview.tsx
@@ -1,25 +1,15 @@
 import type { AssetWithDetails } from "@/lib/types";
 import { useState } from "react";
-import LoadingIndicator from "../../components/LoadingIndicator";
 
-interface AssetPreviewProps {
-  asset: AssetWithDetails;
-}
-
-const AssetPreview = ({ asset }: AssetPreviewProps) => {
+const AssetPreview = ({ asset }: { asset: AssetWithDetails }) => {
   const [imageLoaded, setImageLoaded] = useState(false);
 
   return (
-    <div className="lg:col-span-7 bg-secondary rounded-lg overflow-hidden">
-      {!imageLoaded && (
-        <div className="aspect-video w-full flex items-center justify-center">
-          <LoadingIndicator size="lg" />
-        </div>
-      )}
+    <div className="lg:col-span-7 bg-secondary rounded-lg overflow-hidden h-fit">
       <img
-        src={asset?.thumbnailUrl}
-        alt={asset?.name}
-        className={`w-full aspect-video object-cover transition-opacity duration-500 ${
+        src={asset.thumbnailUrl}
+        alt={asset.name}
+        className={`w-full aspect-square object-cover transition-opacity duration-500 ${
           imageLoaded ? "opacity-100" : "opacity-0"
         }`}
         onLoad={() => setImageLoaded(true)}
